97eab198bce15cdd16a329b74fe4b0616c0f4333
Ilya Alekseenkov <rmfighht@yandex.ru>
2021-08-04 18:57:23 +0300

    Преобразовал тест в три разных теста

--- src/js/__tests__/test-app.js
+++ src/js/__tests__/test-app.js
@@ -1,28 +1,40 @@
 import showHealthLevel from '../app';
 
-test('the result of determining the character`s health level', () => {
-  const inputHealthy = {
+test('the result of determining the character`s health level (healthy)', () => {
+  const input = {
     name: 'Маг',
     health: 90,
   };
-  const inputWounded = {
+
+  const expected = 'healthy';
+
+  const received = showHealthLevel(input);
+
+  expect(received).toBe(expected);
+});
+
+test('the result of determining the character`s health level (wounded)', () => {
+  const input = {
     name: 'Маг',
     health: 45,
   };
-  const inputCritical = {
+
+  const expected = 'wounded';
+
+  const received = showHealthLevel(input);
+
+  expect(received).toBe(expected);
+});
+
+test('the result of determining the character`s health level (critical)', () => {
+  const input = {
     name: 'Маг',
     health: 10,
   };
 
-  const expectedHealthy = 'healthy';
-  const expectedWounded = 'wounded';
-  const expectedCritical = 'critical';
+  const expected = 'critical';
 
-  const receivedHealthy = showHealthLevel(inputHealthy);
-  const receivedWounded = showHealthLevel(inputWounded);
-  const receivedCritical = showHealthLevel(inputCritical);
+  const received = showHealthLevel(input);
 
-  expect(receivedHealthy).toBe(expectedHealthy);
-  expect(receivedWounded).toBe(expectedWounded);
-  expect(receivedCritical).toBe(expectedCritical);
+  expect(received).toBe(expected);
 });
